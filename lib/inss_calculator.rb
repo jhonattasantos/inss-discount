e207e5dc90d3d7c6d7094ecf35b34d50b997655e
Jhonatta Santos <jhonattasantoss@hotmail.com>
2024-04-01 13:36:50 -0300

    add rubocop lint

--- lib/inss_calculator.rb
+++ lib/inss_calculator.rb
@@ -1,14 +1,14 @@
 class InssCalculator
-  def calculate(salary)
-    inss_ranges = [
-      { min_salary: 0.0, max_salary: 1045.00, rate: 0.075 },
-      { min_salary: 1045.01, max_salary: 2089.60, rate: 0.09 },
-      { min_salary: 2089.61, max_salary: 3134.40, rate: 0.12 },
-      { min_salary: 3134.41, max_salary: Float::INFINITY, rate: 0.14 }
-    ]
+  INSS_RANGES = [
+    { min_salary: 0.0, max_salary: 1045.00, rate: 0.075 },
+    { min_salary: 1045.01, max_salary: 2089.60, rate: 0.09 },
+    { min_salary: 2089.61, max_salary: 3134.40, rate: 0.12 },
+    { min_salary: 3134.41, max_salary: Float::INFINITY, rate: 0.14 }
+  ].freeze
 
+  def self.calculate(salary)
     total_discount = 0.0
-    inss_ranges.each do |range|
+    INSS_RANGES.each do |range|
       if salary > range[:max_salary]
         total_discount += ((range[:max_salary] - range[:min_salary]) * range[:rate]).floor(2)
       else
