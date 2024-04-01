04493ef5b596869e609851ea47c4dd14e56cdc31
Jhonatta Santos <jhonattasantoss@hotmail.com>
2024-04-01 13:50:44 -0300

    test: add first and second range

--- spec/lib/inss_calculator_spec.rb
+++ spec/lib/inss_calculator_spec.rb
@@ -2,13 +2,30 @@ require 'rails_helper'
 
 RSpec.describe InssCalculator do
   describe '#calculate' do
+    context 'when salary is within the first range' do
+      it 'returns the correct INSS discount' do
+        salary = 1000
+        expected_discount = 75.0
+
+        expect(described_class.calculate(salary)).to eq(expected_discount)
+      end
+    end
+
+    context 'when salary is within the second range' do
+      it 'returns the correct INSS discount' do
+        salary = 1500
+        expected_discount = 119.31
+
+        expect(described_class.calculate(salary)).to eq(expected_discount)
+      end
+    end
+
     context 'when salary is within the third range' do
       it 'returns the correct INSS discount' do
-        calculator = InssCalculator.new
         salary = 3000
         expected_discount = 281.62
 
-        expect(calculator.calculate(salary)).to eq(expected_discount)
+        expect(described_class.calculate(salary)).to eq(expected_discount)
       end
     end
   end
