941b582296c94e1d82f11fc436aaa4e03387a6f6
Jhonatta Santos <jhonattasantoss@hotmail.com>
2024-04-01 14:43:55 -0300

    test: add rubycritic

--- Gemfile
+++ Gemfile
@@ -53,6 +53,7 @@ group :development, :test do
   gem 'simplecov', require: false
   gem 'factory_bot_rails'
   gem 'faker'
+  gem "rubycritic", require: false
 end
 
 group :development do
--- lib/inss_calculator.rb
+++ lib/inss_calculator.rb
@@ -1,4 +1,6 @@
+# Responsible for calculating INSS deductions based on salary.
 class InssCalculator
+  # INSS rates and salary ranges.
   INSS_RANGES = [
     { min_salary: 0.0, max_salary: 1045.00, rate: 0.075 },
     { min_salary: 1045.01, max_salary: 2089.60, rate: 0.09 },
@@ -6,13 +8,25 @@ class InssCalculator
     { min_salary: 3134.41, max_salary: 6101.06, rate: 0.14 }
   ].freeze
 
+  # Calculates the INSS deduction based on the provided salary.
+  #
+  # Parameters:
+  #   - salary: The salary from which the INSS deduction will be calculated.
+  #
+  # Returns:
+  #   The value of the INSS deduction, rounded to two decimal places.
   def self.calculate(salary)
     total_discount = 0.0
+
     INSS_RANGES.each do |range|
-      if salary > range[:max_salary]
-        total_discount += ((range[:max_salary] - range[:min_salary]) * range[:rate]).floor(2)
+      max_salary = range[:max_salary]
+      min_salary = range[:min_salary]
+      rate = range[:rate]
+
+      if salary > max_salary
+        total_discount += ((max_salary - min_salary) * rate).floor(2)
       else
-        total_discount += ((salary - range[:min_salary]) * range[:rate]).floor(2)
+        total_discount += ((salary - min_salary) * rate).floor(2)
         break
       end
     end
